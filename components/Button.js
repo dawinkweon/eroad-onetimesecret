587d4fbbb7510c3ddd36eb501ae9032c9812ba90
Guilherme Oliveira <guilherme.oliveira@eroad.com>
2022-06-16 18:12:46 +1200

    feat(Button): variants

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,15 @@
-const Button = ({ children, type = 'button', className, onClick }) => {
+import classnames from 'classnames'
+
+const Button = ({ children, type = 'button', className, onClick, variant = 'primary' }) => {
+	const classes = classnames([
+		'drop-shadow-md rounded font-bold p-2',
+		className,
+		{ 'border-2 border-cyan-500 bg-cyan-500': variant === 'primary' },
+		{ 'border-2 border-cyan-500': variant !== 'primary' },
+	])
+
 	return (
-		<button
-			type={type}
-			className={`rounded font-bold bg-cyan-500 p-2 ${className}`}
-			onClick={() => onClick && onClick()}
-		>
+		<button type={type} className={classes} onClick={() => onClick && onClick()}>
 			{children}
 		</button>
 	)
