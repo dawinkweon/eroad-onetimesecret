382d621fca768956f1c81917aadd7ee8c0085bde
Guilherme Oliveira <guilherme.oliveira@NZMB2200098.local>
2022-06-16 12:47:05 +1200

    fix(index): weird styling

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,18 @@ import Head from 'next/head'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col h-screen justify-items-center items-center">
-      <Head>
-        <title>EROAD One Time Secret</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className="flex flex-col h-screen justify-items-center items-center">
+			<Head>
+				<title>EROAD One Time Secret</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>
-        <p className="description">
-          Welcome to EROAD One Time Secret
-        </p>
-      </main>
+			<main className="m-auto">
+				<h1 className="text-3xl">Welcome to EROAD One Time Secret</h1>
+			</main>
 
-      <Footer />
-    </div>
-  )
+			<Footer />
+		</div>
+	)
 }
