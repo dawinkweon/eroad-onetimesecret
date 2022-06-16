70de093e3aa4514b31a7ccae33b018be59959c53
Alvin Smith <alvinsmith.nz@gmail.com>
2022-06-16 15:05:09 +1200

    Initial Crypto

--- pages/api/v1/revealSecret.js
+++ pages/api/v1/revealSecret.js
@@ -1,9 +1,19 @@
+const crypto = require('crypto');
+
 export default function handler(req, res) {
-  console.log(`encrypting secret... secret=[${req.body.secretText}]`);
 
   const post = (req, res) => {
     console.log(`Revealing secret for token = [${req.body.token}]`);
-    res.status(200).json({ secret: "Some Secret" });
+
+    if (!req.body.token) {
+     res.status(400).json({ message: "token is missing in body" });
+    }
+
+    const mykey = crypto.createDecipher('aes-128-cbc', '!fEiDRs@NA6E!V');
+    let mystr = mykey.update(req.body.token, 'hex', 'utf8')
+    mystr += mykey.final('utf8');
+
+    res.status(200).json({ secret: mystr });
   } 
 
   if (req.method === 'POST') {
@@ -11,6 +21,5 @@ export default function handler(req, res) {
   } else {
     res.status(404);
   }
-
 
 }
--- pages/api/v1/secret.js
+++ pages/api/v1/secret.js
@@ -1,13 +1,18 @@
+const crypto = require('crypto');
+
 export default function handler(req, res) {
   console.log(`encrypting secret... secret=[${req.body.secretText}]`);
 
-    const post = (req, res) => {
-    // if (!"secretText" in req.body) {
+  const post = (req, res) => {
     if (!req.body.secretText) {
      res.status(400).json({ message: "secretText is missing in body" });
     }
 
-    res.status(200).json({ secretUrl: 'http://path.to.secret/?token=abc' });
+    const mykey = crypto.createCipher('aes-128-cbc', '!fEiDRs@NA6E!V');
+    let mystr = mykey.update(req.body.secretText, 'utf8', 'hex')
+    mystr += mykey.final('hex');
+
+    res.status(200).json({ secretUrl: 'http://path.to.secret/?token=' + mystr });
   }
 
   if (req.method === 'POST') {
