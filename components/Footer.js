7b7f58a7381862b75aa03b880f001a5fb54f8a76
Guilherme Oliveira <guilherme.oliveira@NZMB2200098.local>
2022-06-16 12:28:14 +1200

    feat: code cleanup

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,9 @@
-import styles from './Footer.module.css'
-
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+      <footer className="w-screen text-center mt-4">
+    <hr />
+      EROAD's 2022 Hackathon
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
 import Head from 'next/head'
-import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="flex flex-col h-screen justify-items-center items-center">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      <main className="flex flex-col items-center m-auto">
+          <h1 className="text-4xl font-bold underline">
+              EROAD's one time secret
+          </h1>
       </main>
 
       <Footer />
