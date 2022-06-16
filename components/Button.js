b5e3338de6b3736669dde0c69552078325f886fd
Guilherme Oliveira <guilherme.oliveira@NZMB2200098.local>
2022-06-16 16:19:13 +1200

    feat(Button): onClick handler

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,10 @@
-const Button = ({ children, type = 'button', className }) => {
+const Button = ({ children, type = 'button', className, onClick }) => {
 	return (
-		<button type={type} className={`rounded font-bold bg-cyan-500 p-2 ${className}`}>
+		<button
+			type={type}
+			className={`rounded font-bold bg-cyan-500 p-2 ${className}`}
+			onClick={() => onClick && onClick()}
+		>
 			{children}
 		</button>
 	)
