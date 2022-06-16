a099e2e68c1a2a5e24fc308eefa6637fdad1b69d
Guilherme Oliveira <guilherme.oliveira@eroad.com>
2022-06-16 18:30:14 +1200

    feat(EroadTitle): add logo

--- components/EroadTitle.js
+++ components/EroadTitle.js
@@ -1,5 +1,8 @@
+import Image from 'next/image'
+
 const EroadTitle = () => (
 	<section>
+		<Image src="/eroad-logo-red.svg" width="64" height="64" />
 		<h1 className="text-3xl font-bold">EROAD</h1>
 		<h2 className="text-xl tracking-widest">secret sharer</h2>
 	</section>
