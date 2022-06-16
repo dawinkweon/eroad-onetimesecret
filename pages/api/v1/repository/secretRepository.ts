bd0e038a1a62d2a1b9507009cb514bda4264b1e0
dawin.kweon <dawin.kweon@eroad.com>
2022-06-16 17:18:32 +1200

    api: fake find secret by id for FE integration

--- pages/api/v1/repository/secretRepository.ts
+++ pages/api/v1/repository/secretRepository.ts
@@ -1,14 +1,21 @@
-const secrets : Array<Secret> = [];
+const secrets: Array<Secret> = []
 
 export const storeSecret = (secret: Secret) => {
-    console.log(`Storing secret... secret=[${JSON.stringify(secret)}]`);
+	console.log(`Storing secret... secret=[${JSON.stringify(secret)}]`)
 
-    secrets.push(secret);
-    
-    console.log(`All secrets=[${JSON.stringify(secrets)}]`);
+	secrets.push(secret)
+
+	console.log(`All secrets=[${JSON.stringify(secrets)}]`)
 }
 
-export const findSecretById = (id: string) : (Secret | undefined) => {
-    console.log(`All secrets=[${JSON.stringify(secrets)}]`);
-    return secrets.find(s => s.id === id);
+export const findSecretById = (id: string): Secret | undefined => {
+	return {
+		id: 'uuid',
+		encryptedText: 'encryptedText',
+		token: 'token',
+		createdDate: '2022-06-16T03:09:58+0000',
+		timeToLiveDuration: 'P3DT4H59M',
+	}
+	// console.log(`All secrets=[${JSON.stringify(secrets)}]`);
+	// return secrets.find(s => s.id === id);
 }
