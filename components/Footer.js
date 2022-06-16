dde58e6eaa990e38f6746516542a9a808aa9ecec
Guilherme Oliveira <guilherme.oliveira@NZMB2200098.local>
2022-06-16 12:47:56 +1200

    chore: run prettier

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
-  return (
-    <>
-      <footer className="w-screen text-center mt-4">
-    <hr />
-      EROAD's 2022 Hackathon
-      </footer>
-    </>
-  )
+	return (
+		<>
+			<footer className="w-screen text-center mt-4">
+				<hr />
+				EROAD's 2022 Hackathon
+			</footer>
+		</>
+	)
 }
