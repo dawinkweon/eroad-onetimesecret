1643370e4b7f3eb49a3ac5ab0fa299106dea60f0
Guilherme Oliveira <guilherme.oliveira@NZMB2200098.local>
2022-06-16 15:14:39 +1200

    feat: loading state to card component

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,22 @@
-const Card = ({ children }) => {
-	return <div className="rounded bg-eroad-red p-4 text-white">{children}</div>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+const Card = ({ children, isLoading = false }) => {
+	return (
+		<div className="rounded bg-eroad-red p-4 text-white">
+			{isLoading ? (
+				<div className="flex w-full">
+					<FontAwesomeIcon
+						className="animate-spin flex m-auto"
+						size="2x"
+						icon={faSpinner}
+					/>
+				</div>
+			) : (
+				children
+			)}
+		</div>
+	)
 }
 
 export { Card }
