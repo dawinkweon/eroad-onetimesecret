10aa4e0b1b6939e33754396cf9bbf0bb0979e181
Alvin Smith <alvinsmith.nz@gmail.com>
2022-06-16 14:35:00 +1200

    Fix Cannot access 'post' before initialization

--- pages/api/v1/revealSecret.js
+++ pages/api/v1/revealSecret.js
@@ -1,14 +1,16 @@
 export default function handler(req, res) {
   console.log(`encrypting secret... secret=[${req.body.secretText}]`);
 
+  const post = (req, res) => {
+    console.log(`Revealing secret for token = [${req.body.token}]`);
+    res.status(200).json({ secret: "Some Secret" });
+  } 
+
   if (req.method === 'POST') {
     post(req, res);
   } else {
     res.status(404);
   }
 
-  const post = (req, res) => {
-    console.log(`Revealing secret for token = [${req.body.token}]`);
-    res.status(200).json({ secret: "Some Secret" });
-  } 
+
 }
